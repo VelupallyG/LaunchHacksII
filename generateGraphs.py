4956fbddc6dda70a829791f4118260f71af56bd1
Gautham Velupally <62814886+VelupallyG@users.noreply.github.com>
2023-04-09 14:12:39 -0500

    2nd webpage

---
+++ generateGraphs.py
@@ -0,0 +1,30 @@
+from pandas_datareader import data, wb
+import pandas as pd
+import numpy as np
+import datetime as dt
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import yfinance as yf
+yf.pdr_override()
+
+start= dt.datetime(2006,1,1)
+end = dt.datetime(2016,1,1)
+
+tickers = ['BAC','C','GS','JPM','MS','WF']
+
+d = {}
+dataFrames = []
+for tick in tickers:
+    d[tick] = data.get_data_yahoo(tick, start, end)
+    dataFrames.append(d[tick])
+
+bank_stocks = pd.concat(dataFrames,axis=1,keys=tickers)
+bank_stocks.columns.names = ['Bank Ticker','Stock Info']
+
+import plotly as py
+import plotly.graph_objs as go
+import cufflinks as cf
+cf.go_offline()
+
+fig = bank_stocks.xs(key='Close',axis=1,level='Stock Info').iplot()
